3aedb3ea1f25000dacb554e1138574424a18f7d3
D4WKINS <78683271+D4WKINS@users.noreply.github.com>
2021-09-08 10:18:11 +0100

    Update SignUp.jsx

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
-import {Form,Button,Col,Row} from 'react-bootstrap'
-import {Link} from 'react-router-dom'
 import {Form,Button,Col,Row,Alert} from 'react-bootstrap'
+
 import {Link,Redirect} from 'react-router-dom'
+
 import {useState} from 'react';
+
 import axios from 'axios'
 
 const FormContainer = styled.div`
@@ -12,17 +13,12 @@ const FormContainer = styled.div`
 max-width:400px;
 margin:3rem auto 0 auto;
 `
-const handleLogin=(e)=>{
-    e.preventDefault()
 
 
-}
-const Login = () => {
 const SignUp = () => {
     const [name,setName] = useState("")
     const [email,setEmail] = useState("")
     // const [phone,setPhone] = useState("")
-const [password,setPassword] = useState("")
     const [password,setPassword] = useState("")
     // const [confirmPassword,setConfirmPassword] = useState("")
     const[error,setError] = useState(false)
@@ -55,20 +51,16 @@ function authenticate(){
     return ( 
         <FormContainer className="px-3 px-md-0 mb-3">
                 <Form>
-                    <Row>
-                        <Col >
+                  
                        
                         {error && <Alert variant="warning"><small>{`An error occurred , please try again later.`}</small></Alert>}
                             <Form.Group>
                                 <h3>Sign Up</h3>
-                                <h3>Sign Up</h3>  
                                 <p style={{color:"black" }}>Have an account already?<Link to="/" style={{color:"#00E676"}}> Login</Link></p>
                             </Form.Group>
-                        </Col>
-                    </Row>
+            
 
                     <Row>
-                        <Col>
                         <Col sm={12} md={6}>
                         <Form.Group className="mb-3">
                             <Form.Label>Name</Form.Label>
@@ -76,26 +68,25 @@ function authenticate(){
                         </Form.Group>
                         </Col>
 
-                        <Col>
                         <Col sm={12} md={6}>
                         <Form.Group className="mb-3">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                         </Form.Group>
                         </Col>
+                  
                     </Row>
                   
 
                     <Row>
-                        <Col>
+                      
                         <Col sm={12} md={6}>
                             <Form.Group className="mb-3">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                                <Form.Control type="password" placeholder="Enter password" value={password} onChange={(e)=> setPassword(e.target.value)}  required/>
                             </Form.Group>
                         </Col>
-                        <Col>
+                      
                         <Col sm={12} md={6}>
                             <Form.Group>
                                 <Form.Label>Confirm password</Form.Label>
@@ -105,10 +96,9 @@ function authenticate(){
                     </Row>
 
                 </Form>
-                 <Button className=" d-block ms-auto mt-3 loginBtn" style={{border:"none",backgroundColor:"black"}} onClick={()=>handleLogin()}  type="submit">Sign Up</Button>
                  <Button className=" d-block ms-auto mt-3 loginBtn" style={{border:"none",backgroundColor:"black"}} onClick={()=>authenticate()}>Sign Up</Button>
             </FormContainer>
      );
 }
  
-export default Login;
+export default SignUp;
